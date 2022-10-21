d229abc8ab1c79834e7e0183b6e6337bf299848c
Lovepreet Kaur <klovepreet140@gmail.com>
2022-10-21 14:10:01 +0530

    addedCmp

---
+++ testproject/src/app/banner-carosuel/banner-carosuel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-carosuel',
+  templateUrl: './banner-carosuel.component.html',
+  styleUrls: ['./banner-carosuel.component.scss']
+})
+export class BannerCarosuelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
